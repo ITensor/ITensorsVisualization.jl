ea9ced02265f3b0a204b42121cd565403cc4fb57
Matthew Fishman <mfishman@flatironinstitute.org>
2021-02-10 17:51:53 -0500

    Add tests

--- examples/visualize.jl
+++ examples/visualize.jl
@@ -14,17 +14,17 @@ x = Index([QN("Sz", 0) => 2]; tags = "X")
 y = Index([QN("Sz", 0) => 2]; tags = "Y")
 
 n = 2
-ψₙₙ₊₁ = randomITensor(l⃗[n-1], s⃗[n], s⃗[n+1], l⃗[n+1], dag(x), dag(y))
-hₙ = randomITensor(dag(h⃗[n-1]), s⃗[n]', dag(s⃗[n]), h⃗[n], x, y)
-hₙ₊₁ = randomITensor(dag(h⃗[n]), s⃗[n+1]', dag(s⃗[n+1]), h⃗[n+1])
-ELₙ₋₁ = randomITensor(l⃗[n-1]', h⃗[n-1], dag(l⃗[n-1]))
-ERₙ₊₁ = randomITensor(l⃗[n+1]', dag(h⃗[n+1]), dag(l⃗[n+1]))
+ψn1n2 = randomITensor(l⃗[n-1], s⃗[n], s⃗[n+1], l⃗[n+1], dag(x), dag(y))
+hn1 = randomITensor(dag(h⃗[n-1]), s⃗[n]', dag(s⃗[n]), h⃗[n], x, y)
+hn2 = randomITensor(dag(h⃗[n]), s⃗[n+1]', dag(s⃗[n+1]), h⃗[n+1])
+ELn0 = randomITensor(l⃗[n-1]', h⃗[n-1], dag(l⃗[n-1]))
+ERn2 = randomITensor(l⃗[n+1]', dag(h⃗[n+1]), dag(l⃗[n+1]))
 
-R = @visualize ELₙ₋₁ * ψₙₙ₊₁ * hₙ * hₙ₊₁ * ERₙ₊₁ pause = true
-@show R ≈ ELₙ₋₁ * ψₙₙ₊₁ * hₙ * hₙ₊₁ * ERₙ₊₁
+R = @visualize ELn0 * ψn1n2 * hn1 * hn2 * ERn2 pause = true
+@show R ≈ ELn0 * ψn1n2 * hn1 * hn2 * ERn2
 
 # Split it up into multiple contractions
-R1 = @visualize ELₙ₋₁ * ψₙₙ₊₁ * hₙ pause = true
-R2 = @visualize R1 * hₙ₊₁ * ERₙ₊₁ pause = true
-@show R2 ≈ ELₙ₋₁ * ψₙₙ₊₁ * hₙ * hₙ₊₁ * ERₙ₊₁
+R1 = @visualize ELn0 * ψn1n2 * hn1 pause = true
+R2 = @visualize R1 * hn2 * ERn2 pause = true
+@show R2 ≈ ELn0 * ψn1n2 * hn1 * hn2 * ERn2
 
